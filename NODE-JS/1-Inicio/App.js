c2cb1199f60e8193d7842a720b7ab6e514454f58
AdrianaKarczewski <123484872+AdrianaKarczewsk@users.noreply.github.com>
2023-05-30 16:23:31 -0300

    ADD

---
+++ NODE-JS/1-Inicio/App.js
@@ -0,0 +1,17 @@
+var calculadora = require("./Calculadora");
+
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(20,40));
+calculadora.nome = "Calculadora da Adri!"
+console.log(calculadora.nome);
+
+/*var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World!");
+console.log("Meu nome é Adriana!");
+console.log("E eu estou aprendendo Node.js com o Guia do programador");
+
+if(mostrarSite){ // Se mostrar site
+    console.log(site); // Printe o site
+}*/
